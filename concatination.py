2840c4f5685762929fd630d12c4e2bd7f75bfca6
Asha-N1 <71083948+Asha-N1@users.noreply.github.com>
2020-10-07 22:46:29 +0530

    Add files via upload

---
+++ concatination.py
@@ -0,0 +1,6 @@
+# concatination of two strings
+
+s1 = "Welcome "
+s2 = "Thank you"
+x = s1 + s2
+print(x)
---
+++ type.py
@@ -0,0 +1,10 @@
+# some examples to type of variable
+
+a = 10
+print(type(a))  # int
+
+b = 20.45
+print(type(b))  # float
+
+c = "Python"
+print(type(c))  # string

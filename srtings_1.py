eae2b7cab6b795a7d9267aab7b2e2e136d928d06
Asha-N1 <71083948+Asha-N1@users.noreply.github.com>
2020-10-08 22:00:36 +0530

    Add files via upload

---
+++ srtings_1.py
@@ -0,0 +1,16 @@
+# string declaration
+a = "python\n" # using double quotes
+print(a)
+
+b = 'program\n'  # using single quotes
+print (b)
+
+# multiline string declaration
+x = """python is high level programing language,
+easy to understand and implement,
+it is open source. """
+print(x)
+
+
+
+
---
+++ string methods.py
@@ -0,0 +1,20 @@
+# string methods
+y = 'python'
+print(y.capitalize())
+
+# counting character appear in string
+x='asha'
+print(x.count('a'))
+
+print(x.find('s')) # finding position of latter in string
+print(x.index('s')) # find and index is same
+
+print(x.isupper()) # checking given string is upper
+print(x.islower()) # checking given string lower
+
+d = 'PYTHON'
+print(x.upper()) # converting to upper case
+print(d.lower()) # converting to lower case
+
+print(d.replace('P', 'S'))
+

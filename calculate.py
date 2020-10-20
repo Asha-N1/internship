bf7b570b8e6e9d09f96ab5befe61b3c9c0201f28
Asha-N1 <71083948+Asha-N1@users.noreply.github.com>
2020-10-20 21:12:17 +0530

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,16 @@
+# creating calculate module
+
+def add(a,b): # addition  function
+    return a + b
+
+
+def sub(a, b): # subtraction function
+    return a - b
+
+
+def mul(a, b): # multiplication function
+    return a * b
+
+
+def div(a, b): # division function
+    return a / b

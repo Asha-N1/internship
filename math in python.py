23977e88fa9ed205b1e055bc5158992a0aeb5296
Asha-N1 <71083948+Asha-N1@users.noreply.github.com>
2020-10-21 21:49:02 +0530

    Add files via upload

---
+++ math in python.py
@@ -0,0 +1,20 @@
+# math function in python
+import math # importing math module
+
+x = math.sqrt(25) # using math performing square root of 25
+print(x)
+
+a = 9.56
+print(math.floor(a)) # floor will give least value
+print(math.ceil(a)) # ceil will give high value
+
+print(math.pow(2,3)) # power function
+print(math.pi) # pi value
+print(math.e)
+
+b=-980.787
+print(abs(b)) # positive value
+
+
+
+

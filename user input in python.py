9ca71c3058a73cb5b9a35ba34c64edee05f1a423
Asha-N1 <71083948+Asha-N1@users.noreply.github.com>
2020-10-23 22:45:21 +0530

    Add files via upload

---
+++ user input in python.py
@@ -0,0 +1,8 @@
+# user input in python
+a = int(input("enter 1st num:")) # user giving input
+b = int(input("enter 2st num:"))
+c = a + b
+print(c)
+
+x = eval(input("enter the expression:")) # we are using eval and providing expression as input
+print(x)
